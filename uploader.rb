cdb0bf799c0eb6999f468618779ecf795622f8a5
Emilio Blanco <emilioeduardob@gmail.com>
2014-01-06 13:48:56 -0300

    POSTs the archived url

--- uploader.rb
+++ uploader.rb
@@ -8,7 +8,7 @@ class Uploader
 
   def upload
     puts "Uploading #{@filename} to #{ENV["AWS_S3_BUCKET"]}..."
-    @s3.buckets[ENV["AWS_S3_BUCKET"]].objects[key].write(file: @filename)
+    @uploaded_file = @s3.buckets[ENV["AWS_S3_BUCKET"]].objects[key].write(file: @filename)
     puts "Finished uploading #{@filename}"
     return true
   rescue Exception => e
@@ -18,7 +18,7 @@ class Uploader
 
   def notify_api
     uri = URI(ENV["API_HOST"] + "/api/streams/#{stream_id}/archived")
-    Net::HTTP.post_form(uri, {})
+    Net::HTTP.post_form(uri, {archived_url: @uploaded_file.public_url})
   rescue
     puts "Cannot connect to #{uri.to_s}"
   end
@@ -28,6 +28,7 @@ class Uploader
     @filename.match(/\d+/).to_s
   end
 
+  # returns the key name for the new resource on S3
   def key
     File.basename(@filename)
   end
